27a564865fb64e1c1df23bbb7592e7a8d1f2bf54
waldenp0nd <20667094+waldenp0nd@users.noreply.github.com>
2017-11-29 13:51:06 +0000

    Update slashcommands.lua

--- slashcommands.lua
+++ slashcommands.lua
@@ -3,21 +3,37 @@
 local addonName, addonTable = ... -- Pull back the AddOn-Local Variables and store them locally.
 -- addonName = "ravMounts"
 -- addonTable = {}
+addonTable.Version = "1.7.2"
+
+
+-- Special formatting for 'Ravenous' messages
+local function prettyPrint(message, full)
+    if not full then
+        full = false
+    else
+        message = message..":"
+    end
+    local prefix = "\124cff5f8aa6ravMounts"..(RAV_version ~= nil and " v"..RAV_version or "")..(full and " " or ":\124r ")
+
+    DEFAULT_CHAT_FRAME:AddMessage(prefix..message)
+end
 
 
 SLASH_RAVMOUNTS1 = "/ravmounts"
 
 
 local function slashHandler(message, editbox)
-    if string.match(message, "include") or message == "i" then
+    if message == "version" or message == "v" then
+        print("You are running: \124cff5f8aa6ravMounts v"..RAV_version)
+    elseif message == "include" or message == "i" then
         RAV_includeSpecials = true
         prettyPrint("Special Mounts will be included in the normal summoning lists.")
         mountListHandler(true, false) -- force recache, do not announce recache
-    elseif string.match(message, "exclude") or message == "e" then
+    elseif message == "exclude" or message == "e" then
         RAV_includeSpecials = false
         prettyPrint("Special Mounts will be excluded from the normal summoning lists.")
         mountListHandler(true, false) -- force recache, do not announce recache
-    elseif string.match(message, "force") or message == "f" then
+    elseif message == "force" or message == "f" then
         mountListHandler(true, true) -- force recache, announce recache
     else
         mountListHandler(false, false) -- do not force recache, do not announce recache
