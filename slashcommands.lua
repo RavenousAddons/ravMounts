ecc48288bc9d2ad5b4610ac6746f85e5933ba003
waldenp0nd <20667094+waldenp0nd@users.noreply.github.com>
2017-12-31 17:19:09 -0400

    Update functions for slash commands

--- slashcommands.lua
+++ slashcommands.lua
@@ -1,22 +1,7 @@
 
 
-local addonName, addonTable = ... -- Pull back the AddOn-Local Variables and store them locally.
--- addonName = "ravMounts"
--- addonTable = {}
-addonTable.Version = "1.7.3"
-
-
--- Special formatting for 'Ravenous' messages
-local function prettyPrint(message, full)
-    if not full then
-        full = false
-    else
-        message = message..":"
-    end
-    local prefix = "\124cff5f8aa6ravMounts"..(RAV_version ~= nil and " v"..RAV_version or "")..(full and " " or ":\124r ")
-
-    DEFAULT_CHAT_FRAME:AddMessage(prefix..message)
-end
+local _, ravMounts = ... -- Pull back the AddOn-Local Variables and store them locally.
+ravMounts.Version = "1.8.0"
 
 
 SLASH_RAVMOUNTS1 = "/ravmounts"
@@ -24,20 +9,20 @@ SLASH_RAVMOUNTS1 = "/ravmounts"
 
 local function slashHandler(message, editbox)
     if message == "version" or message == "v" then
-        print("You are running: \124cff5f8aa6ravMounts v"..RAV_version)
+        print("You are running: \124cff5f8aa6Ravenous Mounts v"..RAV_version)
     elseif message == "include" or message == "i" then
         RAV_includeSpecials = true
-        prettyPrint("Special Mounts will be included in the normal summoning lists.")
-        mountListHandler(true, false) -- force recache, do not announce recache
+        ravMounts.prettyPrint("Special Mounts will be included in the normal summoning lists.")
+        ravMounts.mountListHandler(true, false) -- force recache, do not announce recache
     elseif message == "exclude" or message == "e" then
         RAV_includeSpecials = false
-        prettyPrint("Special Mounts will be excluded from the normal summoning lists.")
-        mountListHandler(true, false) -- force recache, do not announce recache
+        ravMounts.prettyPrint("Special Mounts will be excluded from the normal summoning lists.")
+        ravMounts.mountListHandler(true, false) -- force recache, do not announce recache
     elseif message == "force" or message == "f" then
-        mountListHandler(true, true) -- force recache, announce recache
+        ravMounts.mountListHandler(true, true) -- force recache, announce recache
     else
-        mountListHandler(false, false) -- do not force recache, do not announce recache
-        mountUpHandler(message)
+        ravMounts.mountListHandler(false, false) -- do not force recache, do not announce recache
+        ravMounts.mountUpHandler(message)
     end
 end
 
