ebf64d77f11962707cb1d8616f5b4b9737234070
waldenp0nd <20667094+waldenp0nd@users.noreply.github.com>
2018-08-16 17:12:04 +0100

    Update LibFlyable.lua

--- LibFlyable.lua
+++ LibFlyable.lua
@@ -29,6 +29,7 @@ local spellForContinent = {
     [1191] = -1, -- Ashran (PvP)
     [1265] = -1, -- Tanaan Jungle Intro
     [1463] = -1, -- Helheim Exterior Area
+    [1500] = -1, -- Broken Shore (scenario for DH Vengeance artifact)
     [1669] = -1, -- Argus (mostly OK, few spots are bugged)
 
     -- Unflyable class halls where IsFlyableArea returns true:
@@ -39,20 +40,6 @@ local spellForContinent = {
     [1469] = -1, -- The Heart of Azeroth (Shaman)
     [1107] = -1, -- Dreadscar Rift (Warlock)
     [1479] = -1, -- Skyhold (Warrior)
-}
-
--- Workaround for bug in patch 7.3.5
-local flyableContinents = {
-    -- These continents previously required special spells to fly in.
-    -- All such spells were removed from the game in patch 7.3.5, but
-    -- the IsFlyableArea() API function was not updated accordingly,
-    -- and incorrectly returns false on these continents for characters
-    -- who did not know the appropriate spell before the patch.
-    [  0] = true, -- Eastern Kingdoms (Flight Master's License)
-    [  1] = true, -- Kalimdor (Flight Master's License)
-    [646] = true, -- Deepholm (Flight Master's License)
-    [571] = true, -- Northrend (Cold Weather Flying)
-    [870] = true, -- Pandaria (Wisdom of the Four Winds)
 }
 
 local noFlySubzones = {
@@ -69,9 +56,9 @@ local GetSubZoneText = GetSubZoneText
 local IsFlyableArea = IsFlyableArea
 local IsSpellKnown = IsSpellKnown
 
-function ravMounts.IsFlyableArea()
-    -- if not IsFlyableArea() -- Workaround for bug in patch 7.3.5
-    if noFlySubzones[GetSubZoneText() or ""] then
+function lib.IsFlyableArea()
+    if not IsFlyableArea()
+    or noFlySubzones[GetSubZoneText() or ""] then
         return false
     end
 
@@ -79,15 +66,6 @@ function ravMounts.IsFlyableArea()
     local reqSpell = spellForContinent[instanceMapID]
     if reqSpell then
         return reqSpell > 0 and IsSpellKnown(reqSpell)
-    end
-
-    -- Workaround for bug in patch 7.3.5
-    -- IsFlyableArea() incorrectly reports false in many locations for
-    -- characters who did not have a zone-specific flying spell before
-    -- the patch (which removed all such spells from the game).
-    if not IsFlyableArea() and not flyableContinents[instanceMapID] then
-        -- Continent is not affected by the bug. API is correct.
-        return false
     end
 
     return IsSpellKnown(34090) or IsSpellKnown(34091) or IsSpellKnown(90265)
