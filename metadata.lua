431a05227252fdb7c6ab6eb90428f7d66bb72d3b
Walden <20667094+waldenp0nd@users.noreply.github.com>
2022-01-29 14:08:27 +0000

    Update metadata.lua

--- metadata.lua
+++ metadata.lua
@@ -5,4 +5,3 @@ ns.title = GetAddOnMetadata(ADDON_NAME, "Title")
 ns.notes = GetAddOnMetadata(ADDON_NAME, "Notes")
 ns.version = GetAddOnMetadata(ADDON_NAME, "Version")
 ns.color = "9eb8c9"
-ns.discord = "https://discord.gg/9XeTpbcjzu"
